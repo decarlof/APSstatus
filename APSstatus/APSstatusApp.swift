d56fa1a7a23386b8be50bc39a023b125e803725f
Francesco De Carlo <decarlof@gmail.com>
2025-11-10 17:35:26 -0600

    Initial Commit

---
+++ APSstatus/APSstatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  APSstatusApp.swift
+//  APSstatus
+//
+//  Created by Francesco De Carlo on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct APSstatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
