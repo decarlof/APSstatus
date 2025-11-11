73fec58b3f5ef43c0d8a034efcbed0774dd67542
Francesco De Carlo <decarlof@gmail.com>
2025-11-10 20:57:44 -0600

    tested

--- APSstatus/Views/AboutView.swift
+++ APSstatus/Views/AboutView.swift
@@ -3,9 +3,6 @@ import SwiftUI
 struct AboutView: View {
     var body: some View {
         VStack(spacing: 20) {
-            Text("APSStatus")
-                .font(.largeTitle)
-                .bold()
             
             let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "?"
             Text("Version \(version)")
