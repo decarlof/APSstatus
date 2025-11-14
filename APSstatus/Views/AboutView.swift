866ed66bab4aa1ff160ce5aeca7f3a958dcb6a32
Francesco De Carlo <decarlof@gmail.com>
2025-11-14 15:05:50 -0600

    update authorship.

--- APSstatus/Views/AboutView.swift
+++ APSstatus/Views/AboutView.swift
@@ -9,7 +9,7 @@ struct AboutView: View {
                 .font(.title3)
                 .foregroundColor(.secondary)
             
-            Text("Originally developed by Michael Borland.\nThis is the SwiftUI converted version.")
+            Text("Author: Francesco De Carlo\nOriginally developed by Robert Soliday.\nThis is the SwiftUI converted version.")
                 .multilineTextAlignment(.center)
                 .padding()
             
